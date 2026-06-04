2ab43d21cc42e802e3bf0e0862445edad44cb2ee
xakerbratan-droid <xaker.bratan@gmail.com>
2026-06-04 21:22:07 +0600

    fix: add base for GitHub Pages routing

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   plugins: [react()],
+  base: '/dz/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
